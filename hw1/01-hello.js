ad990aaafc936cf3cd4e0330b92643b5cffe73a9
Alan <alanrjes@gmail.com>
2023-07-08 12:10:54 -0700

    completed hw1 checklist

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "Hi, my name is Alan. I'm a senior CS major at Reed College in Portland, OR."
+  );
 }
