65ef958af92b7767f0423a1f6c7267bcf3980edd
Alan <alanrjes@gmail.com>
2023-08-02 13:08:34 -0700

    ex1 - hello

--- hw3/01-hello.js
+++ hw3/01-hello.js
@@ -1,1 +1,33 @@
-// Add your code here
+// create profile pic element
+let pic = document.createElement("img");
+pic.src = "../images/alan.jpg";
+pic.width = "200";
+pic.alt = "Alan's profile picture";
+// centered
+pic.style.display = "block";
+pic.style.margin = "auto";
+// circular
+pic.style.borderRadius = "100%";
+
+// create bio text elements
+let bio = document.createElement("p");
+// write first line bold
+let firstline = document.createElement("span");
+firstline.textContent = "Hi, my name is Alan.";
+firstline.style.fontWeight = "bold";
+// add the rest of the bio
+bio.append(firstline);
+let etc = document.createElement("span");
+etc.textContent = "I'm a senior CS major at Reed College in Portland, OR.";
+bio.append(document.createElement("br"));
+bio.append(etc);
+// centered
+bio.style.margin = "20px auto";
+bio.style.width = "300px";
+
+// fix title to match hw1
+document.querySelector("h1").textContent = "01 - Hello";
+
+// add picture & bio to DOM
+document.querySelector("main").append(pic);
+document.querySelector("main").append(bio);
