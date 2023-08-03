20ec75bc3d9fe43158d9aba947600808f095cbca
Alan <alanrjes@gmail.com>
2023-08-02 18:26:51 -0700

    ex2 - form

--- hw3/02-form.js
+++ hw3/02-form.js
@@ -1,1 +1,52 @@
-// Add your code here
+// helper function for getting radio selection
+function getPronouns() {
+  for (const option of document.getElementsByName("pronouns")) {
+    if (option.checked) {
+      return option.labels[0].textContent;
+    }
+  }
+}
+
+// listener function for attempted form submission
+function submitForm() {
+  let formdata = [
+    ["Name", document.getElementById("name").value],
+    ["Username", document.getElementById("username").value],
+    ["Email", document.getElementById("email").value],
+    ["Password", document.getElementById("password").value],
+    [
+      "Date of Birth",
+      new Date(document.getElementById("birthdate").value).toLocaleDateString(),
+    ],
+    ["Preferred Pronouns", getPronouns()],
+  ];
+
+  let missingresponses = [];
+  let printresponses = [];
+  for (const field of formdata) {
+    if (field[1] == "" || field[1] == undefined) {
+      // field is not filled out
+      missingresponses.push(field[0]);
+    } else {
+      // field contains a response
+      printresponses.push(field[0] + ": " + field[1]);
+    }
+  }
+
+  if (missingresponses.length) {
+    console.log(
+      "Can't submit form, answer the following fields: " +
+        missingresponses.toString()
+    );
+  } else {
+    for (const printableresponse of printresponses) {
+      console.log(printableresponse);
+    }
+  }
+}
+
+// apply listener to document (& override reloading the page so that it stays in the console and I can actually see it printed)
+document.addEventListener("submit", function (event) {
+  event.preventDefault();
+  submitForm();
+});
